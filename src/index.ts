6cd356edbda3964a24ae88914f263e18406b00a2
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-04-12 22:32:00 +0200

    feat(memo): added memoizeOnce function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './bytes';
 export * from './errors';
 export * from './injection';
+export * from './memo';
 export * from './safely';
 export * from './types';
 export * from './url';
---
+++ src/memo.ts
@@ -0,0 +1,25 @@
+/**
+ * This function takes a function that returns a value and returns a new function that caches the result of the first call. Basically a argument-less memoization.
+ *
+ * @example
+ * ```typescript
+ * const getCwd = memoizeOnce(() => process.cwd());
+ *
+ * // process.cwd() is only called once
+ * console.log(getCwd());
+ * console.log(getCwd());
+ * ```
+ */
+export function memoizeOnce<T>(value: () => T): () => T {
+  let isCalled = false;
+  let cache: T | undefined;
+
+  return () => {
+    if (!isCalled) {
+      isCalled = true;
+      cache = value();
+    }
+
+    return cache;
+  };
+}
