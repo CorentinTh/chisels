b0a048def1a9fb555443a4c081e5ed9b2585ae6e
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-04-29 21:03:16 +0200

    feat(injection): added support for remaining optional args

--- src/injection.test.ts
+++ src/injection.test.ts
@@ -9,17 +9,22 @@ describe('injection', () => {
           foo: ({ a, b }: { a: number; b: number }) => a + b,
           bar: ({ a, c }: { a: number; c: number }) => a + c,
           baz: ({ f }: { f: number }) => f,
+          qux: ({ a, c = 2 }: { a: number; c?: number }) => a + c,
         },
         { a: 1, b: 3 },
       );
 
-      expect(Object.keys(functions)).toEqual(['foo', 'bar', 'baz']);
+      expect(Object.keys(functions)).toEqual(['foo', 'bar', 'baz', 'qux']);
 
-      const { foo, bar, baz } = functions;
+      const { foo, bar, baz, qux } = functions;
 
       expect(foo()).toBe(4);
       expect(bar({ c: 2 })).toBe(3);
       expect(baz({ f: 5 })).toBe(5);
+
+      expect(qux()).toBe(3);
+      expect(qux({})).toBe(3);
+      expect(qux({ c: 4 })).toBe(5);
     });
   });
 });
