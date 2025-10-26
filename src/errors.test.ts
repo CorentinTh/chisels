af177c29495d2b690b3e4097a0c3b09319eb43c7
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-10-26 17:21:34 +0100

    refactor(errors): improve castError to account for objects

--- src/errors.test.ts
+++ src/errors.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable unicorn/error-message */
 import { describe, expect, test } from 'vitest';
 import { castError } from './errors';
 
@@ -7,10 +6,25 @@ describe('errors', () => {
     test('casts unknown errors to Error instances', () => {
       expect(castError('error')).to.eql(new Error('error'));
       expect(castError(123)).to.eql(new Error('123'));
-      expect(castError(null)).to.eql(new Error());
-      expect(castError(undefined)).to.eql(new Error());
+      expect(castError(null)).to.eql(new Error('Unknown error'));
+      expect(castError(undefined)).to.eql(new Error('Unknown error'));
+      expect(castError({})).to.eql(new Error('Unknown error'));
 
       expect(castError(new Error('error'))).to.eql(new Error('error'));
+    });
+
+    test('when given an object with a message property, the message is used for the error', () => {
+      expect(castError({ message: 'foo' })).to.eql(new Error('foo'));
+    });
+
+    test('external properties are assigned to the Error instance', () => {
+      const errorWithProps = castError({ message: 'foo', code: 500, detail: 'bar' });
+
+      expect(errorWithProps).to.eql(Object.assign(new Error('foo'), { code: 500, detail: 'bar' }));
+      expect(errorWithProps).to.be.instanceOf(Error);
+      expect((errorWithProps as Error & Record<string, unknown>).code).to.eql(500);
+      expect((errorWithProps as Error & Record<string, unknown>).detail).to.eql('bar');
+      expect(errorWithProps.message).to.eql('foo');
     });
   });
 });
